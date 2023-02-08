5dd4e083a9f038dba442e1da0ae33cdebea5040d
benrotlevy <benjamin@rotlevy.com>
2023-02-08 23:13:22 +0200

    cors handled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { dataRouter } from "./routes/routes.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use("", dataRouter);
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
